0ba23fd0d25e0a6e11c8d06a4557ffb4b9daf581
Navarro Hernández, Florencio <florencio.navarro@astrazeneca.com>
2023-04-03 13:33:40 -0600

    Line 44 better calling of element find by xpath

--- step_defs/test_web_steps.py
+++ step_defs/test_web_steps.py
@@ -41,7 +41,7 @@ def search_phrase(browser, text):
 def results_have_one(browser, phrase):
     # Phrase can be changed on feature definition file
     xpath = "//div[@id='links']//*[contains(text(), '"+phrase+"')]"
-    results = browser.find_elements(By.XPATH(xpath))
+    results = browser.find_elements(By.XPATH, xpath)
     assert len(results) > 0
 
 
