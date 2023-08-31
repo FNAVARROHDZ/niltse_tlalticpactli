af46099123f0b620b7cba3a25e7fec9e5f1c9ef8
Florencio Navarro <114176350+FNAVARROHDZ@users.noreply.github.com>
2023-08-30 20:27:18 -0600

    Update test_web_steps.py
    Elements find by NAME and ID

--- step_defs/test_web_steps.py
+++ step_defs/test_web_steps.py
@@ -1,5 +1,6 @@
 from pytest_bdd import scenarios, given, when, then
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 scenarios('../features/web.feature')
 
@@ -9,11 +10,11 @@ def ddg_home(browser):
 
 @when('the user searches for "gherkin test case"')
 def search_for(browser):
-    search_input = browser.find_element_by_name('q')
+    search_input = browser.find_element(By.NAME, 'q')
     search_input.send_keys('gherkin test case')
     search_input.send_keys(Keys.RETURN)
 
 @then('results are shown for "gherkin test case"')
 def search_results(browser):
-    links_div = browser.find_element_by_id('links')
+    links_div = browser.find_element(By.ID, 'links')
     assert len(links_div.find_elements_by_xpath('//div')) > 0
